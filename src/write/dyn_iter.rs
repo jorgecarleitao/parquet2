2c82bedaec4efa7672be17836ad6733939f6b02f
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-06-06 16:04:12 +0000

    Allow writing a file from a stream.

---
+++ src/write/dyn_iter.rs
@@ -0,0 +1,25 @@
+pub struct DynIter<'a, V> {
+    iter: Box<dyn Iterator<Item = V> + 'a>,
+}
+
+impl<'iter, V> Iterator for DynIter<'iter, V> {
+    type Item = V;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+impl<'iter, V> DynIter<'iter, V> {
+    pub fn new<I>(iter: I) -> Self
+    where
+        I: Iterator<Item = V> + 'iter,
+    {
+        Self {
+            iter: Box::new(iter),
+        }
+    }
+}
