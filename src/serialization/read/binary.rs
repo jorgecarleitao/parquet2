4c6861923e58c10d60a7657ce72d3744d5943253
Jorge Leitao <jorgecarleitao@gmail.com>
2021-07-03 06:32:47 +0200

    Made levels be read without allocation. (#20)

--- src/serialization/read/binary.rs
+++ src/serialization/read/binary.rs
@@ -1,6 +1,6 @@
 use parquet_format::Encoding;
 
-use super::levels::consume_level;
+use super::levels::{get_bit_width, RLEDecoder};
 use crate::error::{ParquetError, Result};
 use crate::metadata::ColumnDescriptor;
 use crate::read::{Page, PageHeader};
@@ -11,17 +11,15 @@ use crate::{
     read::BinaryPageDict,
 };
 
-fn read_dict_buffer(
-    def_levels: &[u8],
+fn read_dict_buffer_impl<I: Iterator<Item = u32>>(
+    def_levels: I,
     values: &[u8],
     length: u32,
+    max_def_level: u32,
     dict: &BinaryPageDict,
-    def_level_encoding: (&Encoding, i16),
 ) -> Vec<Option<Vec<u8>>> {
     let dict_values = dict.values();
     let dict_offsets = dict.offsets();
-
-    let levels = consume_level(def_levels, length, def_level_encoding);
 
     let bit_width = values[0];
     let values = &values[1..];
@@ -29,21 +27,44 @@ fn read_dict_buffer(
     let (_, consumed) = uleb128::decode(&values);
     let values = &values[consumed..];
 
-    let mut indices = bitpacking::Decoder::new(values, bit_width, length as usize);
+    let indices = bitpacking::Decoder::new(values, bit_width, length as usize);
 
-    let is_valid = levels.into_iter().map(|x| x == def_level_encoding.1 as u32);
-    is_valid
-        .map(|is_valid| {
-            if is_valid {
-                let id = indices.next().unwrap() as usize;
-                let start = dict_offsets[id] as usize;
-                let end = dict_offsets[id + 1] as usize;
-                Some(dict_values[start..end].to_vec())
-            } else {
-                None
-            }
+    let iterator = ValuesDef::new(indices, def_levels, max_def_level);
+
+    iterator
+        .map(|maybe_id| {
+            maybe_id.map(|id| {
+                let start = dict_offsets[id as usize] as usize;
+                let end = dict_offsets[id as usize + 1] as usize;
+                dict_values[start..end].to_vec()
+            })
         })
         .collect()
+}
+
+fn read_dict_buffer(
+    def_levels: &[u8],
+    values: &[u8],
+    length: u32,
+    dict: &BinaryPageDict,
+    def_level_encoding: (&Encoding, i16),
+) -> Vec<Option<Vec<u8>>> {
+    let max_def_level = def_level_encoding.1 as u32;
+    match (def_level_encoding.0, max_def_level == 0) {
+        (Encoding::Rle, true) => read_dict_buffer_impl(
+            std::iter::repeat(0).take(length as usize),
+            values,
+            length,
+            max_def_level,
+            dict,
+        ),
+        (Encoding::Rle, false) => {
+            let num_bits = get_bit_width(def_level_encoding.1);
+            let def_levels = RLEDecoder::new(def_levels, num_bits, length);
+            read_dict_buffer_impl(def_levels, values, length, max_def_level, dict)
+        }
+        _ => todo!(),
+    }
 }
 
 pub fn page_dict_to_vec(
@@ -76,23 +97,39 @@ pub fn page_dict_to_vec(
     }
 }
 
-fn read_plain_buffer(
+fn read_buffer_impl<I: Iterator<Item = u32>>(
+    def_levels: I,
+    values: &[u8],
+    length: u32,
+    max_def_level: u32,
+) -> Vec<Option<Vec<u8>>> {
+    let decoded_values =
+        plain_byte_array::Decoder::new(values, length as usize).map(|bytes| bytes.to_vec());
+
+    ValuesDef::new(decoded_values, def_levels, max_def_level).collect()
+}
+
+fn read_buffer(
     def_levels: &[u8],
     values: &[u8],
     length: u32,
     def_level_encoding: (&Encoding, i16),
 ) -> Vec<Option<Vec<u8>>> {
-    let def_levels = consume_level(def_levels, length, def_level_encoding);
-
-    let decoded_values =
-        plain_byte_array::Decoder::new(values, length as usize).map(|bytes| bytes.to_vec());
-
-    ValuesDef::new(
-        decoded_values,
-        def_levels.into_iter(),
-        def_level_encoding.1 as u32,
-    )
-    .collect()
+    let max_def_level = def_level_encoding.1 as u32;
+    match (def_level_encoding.0, max_def_level == 0) {
+        (Encoding::Rle, true) => read_buffer_impl(
+            std::iter::repeat(0).take(length as usize),
+            values,
+            length,
+            max_def_level,
+        ),
+        (Encoding::Rle, false) => {
+            let num_bits = get_bit_width(def_level_encoding.1);
+            let def_levels = RLEDecoder::new(def_levels, num_bits, length);
+            read_buffer_impl(def_levels, values, length, max_def_level)
+        }
+        _ => todo!(),
+    }
 }
 
 pub fn page_to_vec(page: &Page, descriptor: &ColumnDescriptor) -> Result<Vec<Option<Vec<u8>>>> {
@@ -102,7 +139,7 @@ pub fn page_to_vec(page: &Page, descriptor: &ColumnDescriptor) -> Result<Vec<Opt
             (Encoding::Plain, None) => {
                 let (_, def_levels, values) =
                     levels::split_buffer_v1(page.buffer(), false, descriptor.max_def_level() > 0);
-                Ok(read_plain_buffer(
+                Ok(read_buffer(
                     def_levels,
                     values,
                     page.num_values() as u32,
--- src/serialization/read/levels.rs
+++ src/serialization/read/levels.rs
@@ -3,7 +3,7 @@ use parquet_format::Encoding;
 use crate::encoding::{bitpacking, get_length, hybrid_rle, log2};
 
 #[inline]
-fn get_bit_width(max_level: i16) -> u32 {
+pub fn get_bit_width(max_level: i16) -> u32 {
     log2(max_level as u64)
 }
 
@@ -19,6 +19,93 @@ pub fn decode(values: &[u8], length: u32, encoding: (&Encoding, i16)) -> Vec<u32
             todo!()
         }
         _ => unreachable!(),
+    }
+}
+
+enum DecoderState<'a> {
+    Bitpacking(bitpacking::Decoder<'a>),
+    Rle { value: u32, length: usize },
+    Finished,
+}
+
+pub struct RLEDecoder<'a> {
+    runner: hybrid_rle::Decoder<'a>,
+    state: DecoderState<'a>,
+    num_bits: u32,
+    length: u32,
+    length_so_far: usize,
+}
+
+impl<'a> RLEDecoder<'a> {
+    pub fn new(values: &'a [u8], num_bits: u32, length: u32) -> Self {
+        let runner = hybrid_rle::Decoder::new(&values, num_bits);
+        let mut this = Self {
+            length,
+            num_bits,
+            state: DecoderState::Finished,
+            runner,
+            length_so_far: 0,
+        };
+        this.load_run();
+        this
+    }
+
+    fn load_run(&mut self) {
+        let run = self.runner.next();
+        if let Some(run) = run {
+            match run {
+                hybrid_rle::HybridEncoded::Bitpacked(compressed) => {
+                    let pack_length = (compressed.len() as usize / self.num_bits as usize) * 8;
+                    let additional = std::cmp::min(
+                        self.length_so_far + pack_length,
+                        self.length as usize - self.length_so_far,
+                    );
+                    self.state = DecoderState::Bitpacking(bitpacking::Decoder::new(
+                        compressed,
+                        self.num_bits as u8,
+                        additional,
+                    ));
+                }
+                hybrid_rle::HybridEncoded::Rle(pack, items) => {
+                    let mut bytes = [0u8; std::mem::size_of::<u32>()];
+                    pack.iter()
+                        .enumerate()
+                        .for_each(|(i, byte)| bytes[i] = *byte);
+                    let value = u32::from_le_bytes(bytes);
+                    self.state = DecoderState::Rle {
+                        value,
+                        length: items,
+                    };
+                }
+            }
+        } else {
+            self.state = DecoderState::Finished;
+        }
+    }
+}
+
+impl<'a> Iterator for RLEDecoder<'a> {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next = match &mut self.state {
+            DecoderState::Finished => return None,
+            DecoderState::Bitpacking(decoder) => decoder.next(),
+            DecoderState::Rle { value, length } => {
+                if *length == 0 {
+                    None
+                } else {
+                    *length -= 1;
+                    Some(*value)
+                }
+            }
+        };
+        if next.is_none() {
+            self.load_run()
+        }
+
+        self.length_so_far += 1;
+        next
     }
 }
 
@@ -50,14 +137,6 @@ pub fn rle_decode(values: &[u8], num_bits: u32, length: u32) -> Vec<u32> {
         }
     });
     values
-}
-
-pub fn consume_level(def_levels: &[u8], length: u32, level_encoding: (&Encoding, i16)) -> Vec<u32> {
-    if level_encoding.1 > 0 {
-        decode(def_levels, length, level_encoding)
-    } else {
-        vec![0; length as usize]
-    }
 }
 
 /// returns slices corresponding to (rep, def, values)
