44ae2150dcaa2617ddc0ade1e4901282115a213c
Jörn Horstmann <git@jhorstmann.net>
2021-07-04 20:17:13 +0200

    Tests and fix for get_bit_width function (#24)

--- src/encoding/mod.rs
+++ src/encoding/mod.rs
@@ -17,21 +17,6 @@ pub fn get_length(values: &[u8]) -> u32 {
     u32::from_le_bytes(values[0..4].try_into().unwrap())
 }
 
-/// Returns floor(log2(x))
-#[inline]
-pub fn log2(mut x: u64) -> u32 {
-    if x == 1 {
-        return 1;
-    }
-    x -= 1;
-    let mut result = 0;
-    while x > 0 {
-        x >>= 1;
-        result += 1;
-    }
-    result
-}
-
 /// Returns the ceil of value/divisor
 #[inline]
 pub fn ceil8(value: usize) -> usize {
