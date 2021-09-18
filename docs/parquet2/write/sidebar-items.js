d18fba4c193ad35d23273e9cebc6743432a41175
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-09-18 09:33:07 +0000

    deploy: 4179260a2029f7f4ea3e99b8b9babf7136fd1a65

--- docs/parquet2/write/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["Version",""]],"fn":[["write_file",""]],"mod":[["stream",""]],"struct":[["DynIter","[`DynIter`] is an implementation of a single-threaded, dynamically-typed iterator."],["WriteOptions",""]],"type":[["RowGroupIter",""]]});
