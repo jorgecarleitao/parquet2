a962eb8add7699df919dc5cc28f09dcf5959e69b
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2021-10-15 15:10:19 +0000

    deploy: 258644e3321a9d6af632b1f2548f3d58f2997363

---
+++ docs/parquet2/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["compression",""],["encoding",""],["error","Unofficial implementation of parquet IO in Rust."],["metadata",""],["page",""],["read",""],["schema",""],["statistics",""],["types",""],["write",""]]});
