20ada82ac541c50e8e8c358390db07ba07b8ad08
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-02-11 15:36:28 +0000

    deploy: 25252aabe91167d63e5257932099fd82b1ecee99

---
+++ docs/parquet2/encoding/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Encoding",""]],"fn":[["ceil8","Returns the ceil of value/divisor"],["get_length","Panics"]],"mod":[["bitpacking",""],["delta_bitpacked",""],["delta_byte_array",""],["delta_length_byte_array",""],["hybrid_rle",""],["plain_byte_array",""],["uleb128",""],["zigzag_leb128",""]]});
