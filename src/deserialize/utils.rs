2fc0a780544261a2b97b48bb3342670480fbe8ab
Jorge Leitao <jorgecarleitao@gmail.com>
2022-04-12 06:36:57 +0200

    Improved filtered pages (#120)

--- src/deserialize/utils.rs
+++ src/deserialize/utils.rs
@@ -89,16 +89,19 @@ pub struct SliceFilteredIter<I> {
     selected_rows: VecDeque<Interval>,
     current_remaining: usize,
     current: usize, // position in the slice
+    total_length: usize,
 }
 
 impl<I> SliceFilteredIter<I> {
     /// Return a new [`SliceFilteredIter`]
     pub fn new(iter: I, selected_rows: VecDeque<Interval>) -> Self {
+        let total_length = selected_rows.iter().map(|i| i.length).sum();
         Self {
             iter,
             selected_rows,
             current_remaining: 0,
             current: 0,
+            total_length,
         }
     }
 }
@@ -110,19 +113,26 @@ impl<T, I: Iterator<Item = T>> Iterator for SliceFilteredIter<I> {
     fn next(&mut self) -> Option<Self::Item> {
         if self.current_remaining == 0 {
             if let Some(interval) = self.selected_rows.pop_front() {
-                // skip the hole between the previous start end this start
+                // skip the hole between the previous start and this start
                 // (start + length) - start
                 let item = self.iter.nth(interval.start - self.current);
                 self.current = interval.start + interval.length;
                 self.current_remaining = interval.length - 1;
+                self.total_length -= 1;
                 item
             } else {
                 None
             }
         } else {
             self.current_remaining -= 1;
+            self.total_length -= 1;
             self.iter.next()
         }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.total_length, Some(self.total_length))
     }
 }
 
@@ -143,13 +153,14 @@ mod test {
         ];
 
         let a: VecDeque<Interval> = intervals.clone().into_iter().collect();
-        let a = SliceFilteredIter::new(iter, a);
+        let mut a = SliceFilteredIter::new(iter, a);
 
         let expected: Vec<usize> = intervals
             .into_iter()
             .flat_map(|interval| interval.start..(interval.start + interval.length))
             .collect();
 
-        assert_eq!(expected, a.collect::<Vec<_>>());
+        assert_eq!(expected, a.by_ref().collect::<Vec<_>>());
+        assert_eq!((0, Some(0)), a.size_hint());
     }
 }
