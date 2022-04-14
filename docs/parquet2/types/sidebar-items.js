adaee8953aacd7439630f27097b6f7c043639175
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-04-14 11:23:59 +0000

    deploy: cc8f95e35dca64cf2e6ea9fb00656116f2a3542a

---
+++ docs/parquet2/types/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["decode",""],["int96_to_i64_ns",""],["ord_binary","Returns the ordering of two binary values."]],"trait":[["NativeType","A physical native representation of a Parquet fixed-sized type."]]});
