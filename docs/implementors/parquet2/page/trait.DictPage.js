787c5e95c97013fce5549836ec5aeb480e0ff135
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-04-22 10:52:21 +0000

    deploy: dc05110e9cc994ab6a02393e9b84d9f93c45a73c

--- docs/implementors/parquet2/page/trait.DictPage.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["parquet2"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
