77437d936f8fbf66c782664bf3479da30380b2ee
ygf11 <yanggf23@gmail.com>
2022-05-01 09:42:18 +0000

    Added new_with_page_meta to PageReader

--- src/read/page/reader.rs
+++ src/read/page/reader.rs
@@ -15,6 +15,28 @@ use crate::page::{
 
 use super::PageIterator;
 
+/// This meta is a small part of [`ColumnChunkMetaData`].
+#[derive(Debug, Clone)]
+pub struct PageMetaData {
+    // The number of values in this column chunk.
+    pub num_values: i64,
+    // Compression type
+    pub compression: Compression,
+    // The descriptor of this parquet column
+    pub descriptor: Descriptor,
+}
+
+impl PageMetaData {
+    /// Returns a new [`PageMetaData`].
+    pub fn new(num_values: i64, compression: Compression, descriptor: Descriptor) -> Self {
+        Self {
+            num_values,
+            compression,
+            descriptor,
+        }
+    }
+}
+
 /// Type declaration for a page filter
 pub type PageFilter = Arc<dyn Fn(&Descriptor, &DataPageHeader) -> bool + Send + Sync>;
 
@@ -63,6 +85,27 @@ impl<R: Read> PageReader<R> {
             seen_num_values: 0,
             current_dictionary: None,
             descriptor: column.descriptor().descriptor.clone(),
+            pages_filter,
+            buffer,
+        }
+    }
+
+    /// Create a a new [`PageReader`] with [`PageMetaData`].
+    ///
+    /// It assumes that the reader has been `seeked` to the beginning of `column`.
+    pub fn new_with_page_meta(
+        reader: R,
+        reader_meta: PageMetaData,
+        pages_filter: PageFilter,
+        buffer: Vec<u8>,
+    ) -> Self {
+        Self {
+            reader,
+            total_num_values: reader_meta.num_values,
+            compression: reader_meta.compression,
+            seen_num_values: 0,
+            current_dictionary: None,
+            descriptor: reader_meta.descriptor,
             pages_filter,
             buffer,
         }
