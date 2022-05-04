6efd634763eb09e48b3acd5950faff3252221d72
ygf11 <yanggf23@gmail.com>
2022-05-04 04:10:10 +0000

    Added get_page_stream_with_page_meta

--- src/read/page/stream.rs
+++ src/read/page/stream.rs
@@ -9,7 +9,7 @@ use crate::error::Result;
 use crate::metadata::{ColumnChunkMetaData, Descriptor};
 use crate::page::{CompressedDataPage, ParquetPageHeader};
 
-use super::reader::{finish_page, get_page_header, FinishedPage};
+use super::reader::{finish_page, get_page_header, FinishedPage, PageMetaData};
 use super::PageFilter;
 
 /// Returns a stream of compressed data pages
@@ -19,26 +19,36 @@ pub async fn get_page_stream<'a, RR: AsyncRead + Unpin + Send + AsyncSeek>(
     buffer: Vec<u8>,
     pages_filter: PageFilter,
 ) -> Result<impl Stream<Item = Result<CompressedDataPage>> + 'a> {
-    let (col_start, _) = column_metadata.byte_range();
-    reader.seek(SeekFrom::Start(col_start)).await?;
+    get_page_stream_with_page_meta(column_metadata.into(), reader, buffer, pages_filter).await
+}
+
+/// Returns a stream of compressed data pages with [`PageMetaData`]
+pub async fn get_page_stream_with_page_meta<RR: AsyncRead + Unpin + Send + AsyncSeek>(
+    page_metadata: PageMetaData,
+    reader: &mut RR,
+    buffer: Vec<u8>,
+    pages_filter: PageFilter,
+) -> Result<impl Stream<Item = Result<CompressedDataPage>> + '_> {
+    let column_start = page_metadata.column_start;
+    reader.seek(SeekFrom::Start(column_start)).await?;
     Ok(_get_page_stream(
         reader,
-        column_metadata.num_values(),
-        column_metadata.compression(),
-        &column_metadata.descriptor().descriptor,
+        page_metadata.num_values,
+        page_metadata.compression,
+        page_metadata.descriptor,
         buffer,
         pages_filter,
     ))
 }
 
-fn _get_page_stream<'a, R: AsyncRead + AsyncSeek + Unpin + Send>(
-    reader: &'a mut R,
+fn _get_page_stream<R: AsyncRead + AsyncSeek + Unpin + Send>(
+    reader: &mut R,
     total_num_values: i64,
     compression: Compression,
-    descriptor: &'a Descriptor,
+    descriptor: Descriptor,
     mut buffer: Vec<u8>,
     pages_filter: PageFilter,
-) -> impl Stream<Item = Result<CompressedDataPage>> + 'a {
+) -> impl Stream<Item = Result<CompressedDataPage>> + '_ {
     let mut seen_values = 0i64;
     let mut current_dictionary = None;
     try_stream! {
@@ -52,7 +62,7 @@ fn _get_page_stream<'a, R: AsyncRead + AsyncSeek + Unpin + Send>(
             let read_size = page_header.compressed_page_size as i64;
 
             if let Some(data_header) = data_header {
-                if !pages_filter(descriptor, &data_header) {
+                if !pages_filter(&descriptor, &data_header) {
                     // page to be skipped, we sill need to seek
                     reader.seek(SeekFrom::Current(read_size)).await?;
                     continue
@@ -70,7 +80,7 @@ fn _get_page_stream<'a, R: AsyncRead + AsyncSeek + Unpin + Send>(
                 &mut buffer,
                 compression,
                 &current_dictionary,
-                descriptor,
+                &descriptor,
                 None,
             )?;
 
