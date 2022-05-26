d05a6f9c51b91fd64574a4666183b75b419ecfc3
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-05-26 12:59:08 +0000

    deploy: 792a4191f716b23ed3b2f411ceb0c244ffa30dfd

--- docs/implementors/core/fmt/trait.Display.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["parquet2"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"parquet2/error/enum.Error.html\" title=\"enum parquet2::error::Error\">Error</a>","synthetic":false,"types":["parquet2::error::Error"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
