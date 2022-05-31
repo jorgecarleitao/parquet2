daa97f59ef330f3d6beef5665b261ee1ddc1fa9f
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-05-31 17:54:29 +0000

    deploy: c6e5b34b4a643f6e09f3d1981ee62dfcb509883f

--- docs/implementors/std/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["parquet2"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"parquet2/error/enum.Error.html\" title=\"enum parquet2::error::Error\">Error</a>","synthetic":false,"types":["parquet2::error::Error"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
