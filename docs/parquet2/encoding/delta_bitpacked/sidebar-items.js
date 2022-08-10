13c434a62f1285492ae000d51d508541379d1a67
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-08-10 16:38:37 +0000

    deploy: db4bc77b27aeab724b5f42bed97d516536c4603f

---
+++ docs/parquet2/encoding/delta_bitpacked/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["encode","Encodes an iterator of `i32` according to parquet’s `DELTA_BINARY_PACKED`."]],"struct":[["Decoder","Decoder of parquets’ `DELTA_BINARY_PACKED`. Implements `Iterator<Item = i64>`."]]});
