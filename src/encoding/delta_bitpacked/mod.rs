3ba453fc1f0fe2b78b3af331b078e5710cf15fbb
Jorge C. Leitao <jorgecarleitao@gmail.com>
2022-08-14 21:45:39 +0000

    Fixed delta

--- src/encoding/delta_bitpacked/mod.rs
+++ src/encoding/delta_bitpacked/mod.rs
@@ -32,6 +32,25 @@ mod tests {
         assert_eq!(result, data);
     }
 
+    fn some() {
+        let data = vec![
+            -2147483648,
+            -1777158217,
+            -984917788,
+            -1533539476,
+            -731221386,
+            -1322398478,
+            906736096,
+        ];
+
+        let mut buffer = vec![];
+        encode(data.clone().into_iter(), &mut buffer);
+        let iter = Decoder::new(&buffer);
+
+        let result = iter.collect::<Vec<_>>();
+        assert_eq!(result, data);
+    }
+
     #[test]
     fn more_than_one_block() {
         let mut data = vec![1, 3, -1, 2, 3, 10, 1];
