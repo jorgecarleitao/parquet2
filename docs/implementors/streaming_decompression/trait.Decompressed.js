0d9f6a246d5cfaec91bdd9a673728727b9f29e02
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-08-16 16:38:55 +0000

    deploy: 02a43c831f36117e9bf07c7c2771d0ab7e057e18

---
+++ docs/implementors/streaming_decompression/trait.Decompressed.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["parquet2"] = [{"text":"impl Decompressed for <a class=\"enum\" href=\"parquet2/page/enum.Page.html\" title=\"enum parquet2::page::Page\">Page</a>","synthetic":false,"types":["parquet2::page::Page"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
