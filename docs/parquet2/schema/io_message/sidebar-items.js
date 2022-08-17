341591dd2abb244ae7bee36609df517016a3e808
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-08-17 16:24:15 +0000

    deploy: 4abfaad355d96ed8f656cb0a2d1af490fa17ef25

---
+++ docs/parquet2/schema/io_message/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["from_message","Parses message type as string into a Parquet `ParquetType` which, for example, could be used to extract individual columns. Returns Parquet general error when parsing or validation fails."]]};
