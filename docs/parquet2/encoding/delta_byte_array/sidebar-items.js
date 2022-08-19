ab58a4ff3d46d75abfac8a9304a6999eca84d03b
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-08-19 05:40:48 +0000

    deploy: a6c893137e6e57fe8f2baa32daec903e5fb2b532

---
+++ docs/parquet2/encoding/delta_byte_array/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["encode","Encodes an iterator of according to DELTA_BYTE_ARRAY"]],"struct":[["Decoder","Decodes according to Delta strings, prefixes, lengths and values"]]};
