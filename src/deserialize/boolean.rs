14bff49b8e9e412cad84d8d64c8fabaca8e14a15
Jorge C. Leitao <jorgecarleitao@gmail.com>
2022-08-25 04:10:55 +0000

    Improved deserialize

--- src/deserialize/boolean.rs
+++ src/deserialize/boolean.rs
@@ -1,22 +1,26 @@
+use std::collections::VecDeque;
+
 use crate::{
-    encoding::hybrid_rle::{BitmapIter, HybridRleDecoder},
+    encoding::hybrid_rle::HybridRleDecoder,
     error::Error,
+    indexes::Interval,
     page::{split_buffer, DataPage},
     parquet_bridge::{Encoding, Repetition},
     read::levels::get_bit_width,
 };
 
-use super::{utils::FilteredOptionalPageValidity, OptionalPageValidity, SliceFilteredIter};
+use super::{
+    utils::FilteredOptionalPageValidity, FilteredHybridRleDecoderIter, OptionalPageValidity,
+};
 
 #[derive(Debug)]
 #[allow(clippy::large_enum_variant)]
-pub enum BooleanValuesPageState<'a> {
-    Plain(Plain<'a>),
+pub enum BooleanPageValues<'a> {
+    Plain(Bitmap<'a>),
 }
 
-/// The state of a required [`DataPage`] with a boolean physical type
-#[derive(Debug)]
-pub struct Plain<'a> {
+#[derive(Debug, Clone)]
+pub struct Bitmap<'a> {
     pub values: &'a [u8],
     // invariant: offset <= length;
     pub offset: usize,
@@ -24,7 +28,59 @@ pub struct Plain<'a> {
     pub length: usize,
 }
 
-impl<'a> Plain<'a> {
+impl<'a> Bitmap<'a> {
+    pub fn slice(self, offset: usize, length: usize) -> Self {
+        Self {
+            values: self.values,
+            offset,
+            length,
+        }
+    }
+}
+
+/// An iterator adapter that converts an iterator over items into an iterator over slices of
+/// those N items.
+///
+/// This iterator is best used with iterators that implement `nth` since skipping items
+/// allows this iterator to skip sequences of items without having to call each of them.
+#[derive(Debug, Clone)]
+pub struct FilteredBitmap<'a> {
+    bitmap: Bitmap<'a>,
+    intervals: VecDeque<Interval>,
+    total_length: usize,
+}
+
+impl<'a> FilteredBitmap<'a> {
+    /// Return a new [`SliceFilteredIter`]
+    pub fn new(bitmap: Bitmap<'a>, intervals: VecDeque<Interval>) -> Self {
+        let total_length = intervals.iter().map(|i| i.length).sum();
+        Self {
+            bitmap,
+            intervals,
+            total_length,
+        }
+    }
+}
+
+impl<'a> Iterator for FilteredBitmap<'a> {
+    type Item = Bitmap<'a>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.intervals.pop_front().map(|interval| {
+            let item = self.bitmap.clone().slice(interval.start, interval.length);
+            self.total_length -= 1;
+            item
+        })
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.total_length, Some(self.total_length))
+    }
+}
+
+impl<'a> Bitmap<'a> {
     pub fn try_new(page: &'a DataPage) -> Result<Self, Error> {
         let (_, _, values) = split_buffer(page)?;
 
@@ -47,10 +103,10 @@ impl<'a> Plain<'a> {
     }
 }
 
-impl<'a> BooleanValuesPageState<'a> {
+impl<'a> BooleanPageValues<'a> {
     pub fn try_new(page: &'a DataPage) -> Result<Self, Error> {
         match page.encoding() {
-            Encoding::Plain => Plain::try_new(page).map(Self::Plain),
+            Encoding::Plain => Bitmap::try_new(page).map(Self::Plain),
             _ => Err(Error::InvalidParameter(format!(
                 "Viewing page for encoding {:?} for boolean type not supported",
                 page.encoding(),
@@ -61,15 +117,15 @@ impl<'a> BooleanValuesPageState<'a> {
 
 #[derive(Debug)]
 #[allow(clippy::large_enum_variant)]
-pub enum BooleanPageState<'a> {
-    Optional(OptionalPageValidity<'a>, BooleanValuesPageState<'a>),
-    Required(BooleanValuesPageState<'a>),
-    Levels(HybridRleDecoder<'a>, u32, BooleanValuesPageState<'a>),
+pub enum NominalBooleanPage<'a> {
+    Optional(OptionalPageValidity<'a>, BooleanPageValues<'a>),
+    Required(BooleanPageValues<'a>),
+    Levels(HybridRleDecoder<'a>, u32, BooleanPageValues<'a>),
 }
 
-impl<'a> BooleanPageState<'a> {
+impl<'a> NominalBooleanPage<'a> {
     pub fn try_new(page: &'a DataPage) -> Result<Self, Error> {
-        let values = BooleanValuesPageState::try_new(page)?;
+        let values = BooleanPageValues::try_new(page)?;
 
         if page.descriptor.max_def_level > 1 {
             let (_, def_levels, _) = split_buffer(page)?;
@@ -88,9 +144,9 @@ impl<'a> BooleanPageState<'a> {
         match (page.encoding(), is_optional) {
             (Encoding::Plain, true) => Ok(Self::Optional(
                 OptionalPageValidity::try_new(page)?,
-                BooleanValuesPageState::try_new(page)?,
+                BooleanPageValues::try_new(page)?,
             )),
-            (Encoding::Plain, false) => BooleanValuesPageState::try_new(page).map(Self::Required),
+            (Encoding::Plain, false) => BooleanPageValues::try_new(page).map(Self::Required),
             (other, _) => Err(Error::OutOfSpec(format!(
                 "boolean-encoded non-nested pages cannot be encoded as {other:?}"
             ))),
@@ -99,9 +155,67 @@ impl<'a> BooleanPageState<'a> {
 }
 
 #[derive(Debug)]
+pub enum FilteredBooleanPageValues<'a> {
+    Plain(FilteredBitmap<'a>),
+}
+
+impl<'a> FilteredBooleanPageValues<'a> {
+    pub fn new(page: BooleanPageValues<'a>, intervals: VecDeque<Interval>) -> Self {
+        match page {
+            BooleanPageValues::Plain(bitmap) => Self::Plain(FilteredBitmap::new(bitmap, intervals)),
+        }
+    }
+}
+
+#[derive(Debug)]
 #[allow(clippy::large_enum_variant)]
-pub enum FilteredBooleanPageState<'a> {
-    Optional(FilteredOptionalPageValidity<'a>, BitmapIter<'a>),
-    Required(SliceFilteredIter<BitmapIter<'a>>),
-    Levels(HybridRleDecoder<'a>, u32, BitmapIter<'a>),
+pub enum FilteredBooleanPage<'a> {
+    Optional(FilteredOptionalPageValidity<'a>, BooleanPageValues<'a>),
+    Required(FilteredBooleanPageValues<'a>),
+}
+
+impl<'a> FilteredBooleanPage<'a> {
+    pub fn try_new(
+        page: NominalBooleanPage<'a>,
+        intervals: VecDeque<Interval>,
+    ) -> Result<Self, Error> {
+        Ok(match page {
+            NominalBooleanPage::Optional(iter, values) => Self::Optional(
+                FilteredOptionalPageValidity::new(FilteredHybridRleDecoderIter::new(
+                    iter.iter, intervals,
+                )),
+                values,
+            ),
+            NominalBooleanPage::Required(values) => {
+                Self::Required(FilteredBooleanPageValues::new(values, intervals))
+            }
+            NominalBooleanPage::Levels(_, _, _) => {
+                return Err(Error::FeatureNotSupported("Filtered levels".to_string()))
+            }
+        })
+    }
+}
+
+/// The deserialization state of a [`DataPage`] of a parquet binary type
+#[derive(Debug)]
+#[allow(clippy::large_enum_variant)]
+pub enum BooleanPage<'a> {
+    Nominal(NominalBooleanPage<'a>),
+    Filtered(FilteredBooleanPage<'a>),
+}
+
+impl<'a> BooleanPage<'a> {
+    /// Tries to create [`BooleanPage`]
+    /// # Error
+    /// Errors iff the page is not a `BooleanPage`
+    pub fn try_new(page: &'a DataPage) -> Result<Self, Error> {
+        let native_page = NominalBooleanPage::try_new(page)?;
+
+        if let Some(selected_rows) = page.selected_rows() {
+            FilteredBooleanPage::try_new(native_page, selected_rows.iter().copied().collect())
+                .map(Self::Filtered)
+        } else {
+            Ok(Self::Nominal(native_page))
+        }
+    }
 }
--- tests/it/read/boolean.rs
+++ tests/it/read/boolean.rs
@@ -1,4 +1,4 @@
-use parquet2::deserialize::{BooleanPageState, BooleanValuesPageState};
+use parquet2::deserialize::{BooleanPage, BooleanPageValues, NominalBooleanPage};
 use parquet2::encoding::hybrid_rle::BitmapIter;
 use parquet2::error::Result;
 use parquet2::page::DataPage;
@@ -7,31 +7,34 @@ use crate::read::utils::{deserialize_levels, deserialize_optional};
 
 pub fn page_to_vec(page: &DataPage) -> Result<Vec<Option<bool>>> {
     assert_eq!(page.descriptor.max_rep_level, 0);
-    let state = BooleanPageState::try_new(page)?;
+    let state = BooleanPage::try_new(page)?;
 
     match state {
-        BooleanPageState::Optional(validity, values) => match values {
-            BooleanValuesPageState::Plain(values) => {
-                let values = BitmapIter::new(values.values, values.offset, values.length);
-                deserialize_optional(validity, values.map(Ok))
-            }
-        },
-        BooleanPageState::Required(values) => match values {
-            BooleanValuesPageState::Plain(values) => {
-                Ok(BitmapIter::new(values.values, values.offset, values.length)
-                    .into_iter()
-                    .map(Some)
-                    .collect())
-            }
-        },
-        BooleanPageState::Levels(validity, max, values) => {
-            let validity = validity.map(|def| Ok(def? == max));
-            match values {
-                BooleanValuesPageState::Plain(values) => {
+        BooleanPage::Nominal(values) => match values {
+            NominalBooleanPage::Optional(validity, values) => match values {
+                BooleanPageValues::Plain(values) => {
                     let values = BitmapIter::new(values.values, values.offset, values.length);
-                    deserialize_levels(validity, values.map(Ok))
+                    deserialize_optional(validity, values.map(Ok))
+                }
+            },
+            NominalBooleanPage::Required(values) => match values {
+                BooleanPageValues::Plain(values) => {
+                    Ok(BitmapIter::new(values.values, values.offset, values.length)
+                        .into_iter()
+                        .map(Some)
+                        .collect())
+                }
+            },
+            NominalBooleanPage::Levels(validity, max, values) => {
+                let validity = validity.map(|def| Ok(def? == max));
+                match values {
+                    BooleanPageValues::Plain(values) => {
+                        let values = BitmapIter::new(values.values, values.offset, values.length);
+                        deserialize_levels(validity, values.map(Ok))
+                    }
                 }
             }
-        }
+        },
+        BooleanPage::Filtered(..) => todo!(),
     }
 }
