9af995343eb95ab0ce01cfeecc122d54a8d627fc
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-09-02 06:00:38 +0000

    deploy: 4cbce012cfe8477bf231caf2fd683871262ba0d4

---
+++ docs/parquet2/schema/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["Repetition","The repetition of a parquet field"]],"mod":[["io_message",""],["io_thrift",""],["types",""]],"struct":[["SchemaElement","Represents a element inside a schema definition."]]};
---
+++ docs/parquet2/types/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["decode",""],["int96_to_i64_ns",""],["ord_binary","Returns the ordering of two binary values."]],"trait":[["NativeType","A physical native representation of a Parquet fixed-sized type."]]};
