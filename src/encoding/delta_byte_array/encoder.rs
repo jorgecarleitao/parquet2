e3ef972f88c91ff5b3fe9244bf3a629959adac77
Jorge C. Leitao <jorgecarleitao@gmail.com>
2022-11-13 04:32:30 +0000

    Fixed clippy

--- src/encoding/delta_byte_array/encoder.rs
+++ src/encoding/delta_byte_array/encoder.rs
@@ -15,10 +15,7 @@ pub fn encode<'a, I: Iterator<Item = &'a [u8]> + Clone>(iterator: I, buffer: &mu
                 .zip(previous.iter())
                 .enumerate()
                 // find first difference
-                .find_map(|(length, (lhs, rhs))| {
-                    println!("{lhs} {rhs}");
-                    (lhs != rhs).then(|| length)
-                })
+                .find_map(|(length, (lhs, rhs))| (lhs != rhs).then(|| length))
                 .unwrap_or(previous.len());
             previous = item;
 
