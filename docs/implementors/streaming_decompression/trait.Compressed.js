e8b443f1aa258cc3b55589287458cda4b362ad3e
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2022-11-29 06:47:31 +0000

    deploy: 92c0af63956160a67eacd485e1af6c85483350b3

--- docs/implementors/streaming_decompression/trait.Compressed.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"parquet2":[["impl Compressed for <a class=\"enum\" href=\"parquet2/page/enum.CompressedPage.html\" title=\"enum parquet2::page::CompressedPage\">CompressedPage</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
