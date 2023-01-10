c4635584ba05c9744162391407c07330853bf246
hezheyu <rinchannow@bupt.edu.cn>
2023-01-10 17:12:37 +0800

    Fix reimport.

--- src/encoding/delta_byte_array/encoder.rs
+++ src/encoding/delta_byte_array/encoder.rs
@@ -15,7 +15,7 @@ pub fn encode<'a, I: Iterator<Item = &'a [u8]> + Clone>(iterator: I, buffer: &mu
                 .zip(previous.iter())
                 .enumerate()
                 // find first difference
-                .find_map(|(length, (lhs, rhs))| (lhs != rhs).then(|| length))
+                .find_map(|(length, (lhs, rhs))| (lhs != rhs).then_some(length))
                 .unwrap_or(previous.len());
             previous = item;
 
