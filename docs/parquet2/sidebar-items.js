989c37da9cf861ca7058c58621f3fcb8c661f503
jorgecarleitao <jorgecarleitao@users.noreply.github.com>
2023-02-28 07:15:30 +0000

    deploy: 9eee7eb1130aed4cd0d841e59c325cc366fc3798

---
+++ docs/parquet2/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["bloom_filter","compression","deserialize","encoding","error","indexes","metadata","page","read","schema","statistics","types","write"],"trait":["FallibleStreamingIterator"]};
---
+++ docs/parquet2/types/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["decode","int96_to_i64_ns","ord_binary"],"trait":["NativeType"]};
